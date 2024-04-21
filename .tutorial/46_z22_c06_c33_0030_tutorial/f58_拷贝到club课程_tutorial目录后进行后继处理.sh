876f66c24eb86ede12bfb9738214cf4d9913e347
mymy20220326 <mymy20220326@outlook.com>
2024-04-21 11:53:38 +0000

    2024_04_21_0580-wmtag_memo_更新到_46版本_取消了_help_与_env_两个命令_增加了_d22_文件

--- .tutorial/46_z22_c06_c33_0030_tutorial/f58_拷贝到club课程_tutorial目录后进行后继处理.sh
+++ .tutorial/46_z22_c06_c33_0030_tutorial/f58_拷贝到club课程_tutorial目录后进行后继处理.sh
@@ -8,7 +8,7 @@ export v50_grade_directory=../cs36_learning_grade
 
 l30_change_catalog_directory_name(){
     if [[ -f $v38_output_catalog_yml_file ]]; then
-        cp $v38_output_catalog_yml_file $v40_change_from_v38_yml_file
+        cp -f $v38_output_catalog_yml_file $v40_change_from_v38_yml_file
         sed -i 's/_wmgitignore//' $v40_change_from_v38_yml_file
     fi
     return 0
@@ -23,7 +23,7 @@ l34_copy_d40_to_tutorial_directory(){
 
     if [[ -f $v40_change_from_v38_yml_file ]]; then
         # cp $v40_change_from_v38_yml_file ../$v40_change_from_v38_yml_file
-        cp $v40_change_from_v38_yml_file ../$catalog_file_name
+        cp -f $v40_change_from_v38_yml_file ../$catalog_file_name
     fi
 
     # NOTE 把自身也拷贝出去
@@ -50,12 +50,26 @@ l38_copy_grade_code_template_file_to_grade_directory(){
     return 0
 }
 
+l58_copy_other_files_to_tutorial_directory(){
+
+    export v12_tutorail_data_d22=d22_给d18增加前缀.txt
+
+    # NOTE 拷贝到上层目录中去
+    if [[ -f $v12_tutorail_data_d22 ]]; then
+        cp -f $v12_tutorail_data_d22 ../$v12_tutorail_data_d22
+    fi
+
+    return 0
+}
+
 main(){
     l30_change_catalog_directory_name
 
     l34_copy_d40_to_tutorial_directory
 
     l38_copy_grade_code_template_file_to_grade_directory
+
+    l58_copy_other_files_to_tutorial_directory
 
     return 0
 }
