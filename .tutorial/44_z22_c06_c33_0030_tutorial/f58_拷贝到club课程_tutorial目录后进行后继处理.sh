53bc590e029453dc0838e3c4dedbaaa16b7bac65
mymy20220326 <mymy20220326@outlook.com>
2024-04-21 17:37:45 +0800

    2024_04_21_0570-wmtag_memo_拷贝_d46_模版_code_写入bash脚本_到_grade_评分_目录下

--- .tutorial/44_z22_c06_c33_0030_tutorial/f58_拷贝到club课程_tutorial目录后进行后继处理.sh
+++ .tutorial/44_z22_c06_c33_0030_tutorial/f58_拷贝到club课程_tutorial目录后进行后继处理.sh
@@ -3,6 +3,8 @@
 export catalog_file_name=catalog.yml
 export v38_output_catalog_yml_file=d38_catalog.yml
 export v40_change_from_v38_yml_file=d40_catalog_current.yml
+export v46_code_template_file=d46_模版_code_写入bash脚本.sh
+export v50_grade_directory=../cs36_learning_grade
 
 l30_change_catalog_directory_name(){
     if [[ -f $v38_output_catalog_yml_file ]]; then
@@ -15,7 +17,7 @@ l30_change_catalog_directory_name(){
 l34_copy_d40_to_tutorial_directory(){
 
 
-    # if [[ -f $v38_output_catalog_yml_file ]]; then 
+    # if [[ -f $v38_output_catalog_yml_file ]]; then
     #     cp $v38_output_catalog_yml_file ../$v38_output_catalog_yml_file
     # fi
 
@@ -33,10 +35,27 @@ l34_copy_d40_to_tutorial_directory(){
     return 0
 }
 
+l38_copy_grade_code_template_file_to_grade_directory(){
+
+    # NOTE 如果存在_代码的模版文件_拷贝到_评分目录中去
+    # NOTE 在评分的时候_用md5sum_把该_代码的模版文件_进行处理_用于判断_答案_文件是否给学员给更改过
+    if [[ -f $v46_code_template_file ]]; then
+        if [[ -d $v50_grade_directory ]]; then
+            cp -f $v46_code_template_file $v50_grade_directory/
+        else
+            mkdir -p $v50_grade_directory
+            cp -f $v46_code_template_file $v50_grade_directory/
+        fi
+    fi
+    return 0
+}
+
 main(){
     l30_change_catalog_directory_name
 
     l34_copy_d40_to_tutorial_directory
+
+    l38_copy_grade_code_template_file_to_grade_directory
 
     return 0
 }
