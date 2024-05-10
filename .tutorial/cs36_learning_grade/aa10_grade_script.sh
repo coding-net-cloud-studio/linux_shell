e763b518a551420b6f52b53d926cefcfad1f66a4
mymy20220326 <mymy20220326@outlook.com>
2024-05-10 16:21:48 +0800

    2024_05_10_0970-wmtag_memo_升级_评分脚本_aa10_grade_script_脚本_基本完毕

--- .tutorial/cs36_learning_grade/aa10_grade_script.sh
+++ .tutorial/cs36_learning_grade/aa10_grade_script.sh
@@ -193,6 +193,7 @@ f42_show_medal_to_leaner(){
 
 
 # =======================================================================
+# REVIEW 本评分函数_适用于_是在_嵌入_b60_demo_到_b40_code_之前
 # NOTE 当前需要被_评分的_文件_的路径
 # NOTE 该文件_可能_被学员_修改过
 # NOTE 该文件_也可能_没有被学员修改过
@@ -228,6 +229,88 @@ f92_grade_by_student_answer_file(){
 
 
 # =======================================================================
+# NOTE 本函数被下面的_pf24_20_grade_by_student_answer_file_after_embedding_b60_demo_into_b40_code()函数_所调用
+# NOTE 传入一个md5sum的值_作为_$1
+pe20_30_judge_by_compare_with_original_md5sum_value(){
+
+    export v22_10_md5sum_of_origin_tutorial_file=$1
+    export v22_20_md5sum_of_student_file=$(md5sum ${current_need_to_grade_file_path} | awk '{print $1}')
+
+    if [[ ${v22_10_md5sum_of_origin_tutorial_file} == ${v22_20_md5sum_of_student_file} ]]; then
+        echo "学员_同学_你并没有_修改_该答题文件啊! 我们是通过md5sum判断的"
+
+        f32_show_demo_to_leaner
+
+    else
+        echo "学员_同学_对于_答题文件_进行了修改_可以去进行评分"
+
+        f42_show_medal_to_leaner
+
+    fi
+
+    unset v22_10_md5sum_of_origin_tutorial_file
+    unset v22_20_md5sum_of_student_file
+
+
+    return 0
+}
+
+# -----------------------------------------------------------------------
+
+# REVIEW 本评分函数_适用于_是在_嵌入_b60_demo_到_b40_code_以后
+# NOTE 当前需要被_评分的_文件_的路径
+# NOTE 该文件_可能_被学员_修改过
+# NOTE 该文件_也可能_没有被学员修改过
+# NOTE 需要不同的评分_方法_需要返回不同的信息
+pf24_20_grade_by_student_answer_file_after_embedding_b60_demo_into_b40_code(){
+
+    if [[ -f $current_need_to_grade_file_path ]]; then
+
+        # NOTE 取出_类似_如下的内容_
+        local directory_path=$(dirname ${current_need_to_grade_file_path})
+
+        # NOTE 取出_类似_如下的内容_c014_ls_展示目录中文件及其属性信息
+        local directory_name=$(basename $(dirname ${current_need_to_grade_file_path}) )
+
+        # NOTE 取出_类似_如下的内容_ls.sh
+        local short_file_name=$(basename ${current_need_to_grade_file_path})
+
+        #  NOTE 拼接出来一个_特征格式_类似如下_"c014_ls_展示目录中文件及其属性信息###ls.sh"
+        local md5sum_mark="${directory_name}###${short_file_name}"
+
+        local the_file_judge_file_name="还没有给文件名赋值"
+
+        local the_file_original_md5sum="还没有赋值"
+
+        if [[ $(find ${directory_path} -type f -name "*${md5sum_mark}*" | wc -l ) -gt 0 ]]; then
+            # NOTE 获取到类似如下的文件名_获得第1个文件_就可以了
+            # vv20_md5sum###c014_ls_展示目录中文件及其属性信息###ls.sh###7e2a6c0742a74b528de72d40bc8f10f5
+            the_file_judge_file_name=$(ls $directory_path/*$md5sum_mark* | head -n 1)
+            echo -e "pf24_20_2030_the_file_judge_file_name: ${the_file_judge_file_name}\n"
+            # NOTE 拆分上述文件名称_提取其中的信息_提取的是_$4_也就是最后一段信息_$NF
+            # 提取到的信息_类似如下
+            # "7e2a6c0742a74b528de72d40bc8f10f5"
+            # NOTE 这就是_原始答题纸文件的_原始_md5sum_信息
+            the_file_original_md5sum=$(echo $the_file_judge_file_name | awk -F"###" '{print $NF}')
+            if [[ ${the_file_original_md5sum}  != "还没有赋值" ]]; then
+                # NOTE 调用函数_去做判断
+                pe20_30_judge_by_compare_with_original_md5sum_value ${the_file_original_md5sum}
+            else
+                echo "pf24_20_3030_判断依据出现问题_无法判断_学员_是否修改过_该文件"
+            fi
+
+        else
+            echo "pf24_20_4060_无法找到_判断的依据_无法判断_学员_是否修改过_该文件"
+
+        fi
+
+    fi
+
+    return 0
+}
+
+
+# =======================================================================
 
 main(){
 
@@ -239,8 +322,11 @@ main(){
     # md5sum ${answer_sheet_original_bash_script_file}
     # md5sum ${current_need_to_grade_file_path}
 
+    # REVIEW 本评分函数_适用于_是在_嵌入_b60_demo_到_b40_code_之前
+    # f92_grade_by_student_answer_file
 
-    f92_grade_by_student_answer_file
+    # REVIEW 本评分函数_适用于_是在_嵌入_b60_demo_到_b40_code_以后
+    pf24_20_grade_by_student_answer_file_after_embedding_b60_demo_into_b40_code
 
     return 0
 }
